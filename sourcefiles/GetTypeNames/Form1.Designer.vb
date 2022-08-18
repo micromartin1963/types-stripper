8907c05b6f13159669a2ac977c429353b2940679
micromartin1963 <micromartin1963@gmail.com>
2022-08-18 15:14:21 +0100

    Add files via upload

---
+++ sourcefiles/GetTypeNames/Form1.Designer.vb
@@ -0,0 +1,152 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 28)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(119, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Get File Name"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(162, 29)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(610, 23)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 88)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(109, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Get File contents"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(172, 104)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox2.Size = New System.Drawing.Size(382, 308)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 143)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Strip"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(609, 88)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox3.Size = New System.Drawing.Size(332, 383)
+        Me.TextBox3.TabIndex = 5
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(172, 458)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(276, 23)
+        Me.TextBox4.TabIndex = 6
+        Me.TextBox4.Text = "Number of types = ?"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(974, 31)
+        Me.TextBox5.Multiline = True
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox5.Size = New System.Drawing.Size(355, 440)
+        Me.TextBox5.TabIndex = 7
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(27, 265)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "save"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1386, 530)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+End Class
---
+++ sourcefiles/GetTypeNames/Form1.vb
@@ -0,0 +1,124 @@
+﻿
+
+
+Imports System.IO
+
+Public Class Form1
+
+
+    Dim NumberOfTypes As Integer
+    Dim PresentType As Integer = 0
+    Dim PresentTypeText As String = ""
+    Dim EndText As String
+    Dim AllText As String
+    Dim fileReader As String
+    Dim counter As Integer = 0
+
+
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Do_Stuff()
+    End Sub
+
+    Private Sub Do_Stuff()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Using O As New OpenFileDialog With {.Filter = "Text Files|*.txt;*.xml"}
+            If O.ShowDialog = 1 Then
+                TextBox1.Text = O.FileName
+            End If
+        End Using
+
+        '  Dim objReader As New System.IO.StreamReader(FILE_NAME)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        fileReader = My.Computer.FileSystem.ReadAllText(TextBox1.Text)
+        TextBox2.Text = fileReader
+    End Sub
+
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        GetNumberOfTypes()
+        Pass001()
+        Pass002()
+
+
+
+    End Sub
+
+    Private Sub Pass001()
+
+
+
+        Dim reader As StreamReader = My.Computer.FileSystem.OpenTextFileReader(TextBox1.Text)
+        Dim a As String
+
+        Do
+            a = reader.ReadLine
+            If a.Contains("type name") = True Then
+                TextBox3.Text = TextBox3.Text + vbCrLf + a
+                counter = counter + 1
+            Else
+
+            End If
+
+        Loop Until counter = NumberOfTypes
+
+    End Sub
+
+
+    Private Sub Pass002()
+        Dim str As String
+        str = TextBox3.Text
+        Dim repStr As String = str.Replace("<type name=", "")
+        repStr = repStr.Replace("""", "")
+        repStr = repStr.Replace(">", "")
+        repStr = repStr.Replace(" ", "")
+        repStr = repStr.Replace(vbTab, "")
+        TextBox5.Text = repStr
+    End Sub
+
+
+
+
+
+    Private Sub WriteALine(str As String, sw As StreamWriter)
+
+
+        Using sw
+            sw.WriteLine(str)
+        End Using
+
+
+    End Sub
+
+
+
+
+
+    Private Sub GetNumberOfTypes()
+        Dim mysplit As Array
+        mysplit = Split(fileReader, "type name")
+        '  MsgBox(mysplit.Length - 1)
+        NumberOfTypes = mysplit.Length - 1
+        TextBox4.Text = "Number of types   " + (mysplit.Length - 1).ToString
+
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        SaveFileDialog1.Filter = "TXT Files (*.txt*)|*.txt"
+        If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK _
+         Then
+            My.Computer.FileSystem.WriteAllText _
+            (SaveFileDialog1.FileName, TextBox5.Text, True)
+        End If
+    End Sub
+End Class
